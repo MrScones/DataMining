47884cf7bace18ceee48e81f0f9b2aea94fbed35
MrScones <jond@itu.dk>
2013-05-01 11:54:33 +0200

    Equal width binning

---
+++ FinalProject/DataMining/Preprocessing/EqualWidthBinLabeling.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMining.Preprocessing
+{
+    public class EqualWidthBinLabeling : IDataProcessor
+    {
+        private readonly string _columnName;
+        private readonly int _binWidth;
+
+        public EqualWidthBinLabeling(string columnName, int binWidth)
+        {
+            _columnName = columnName;
+            _binWidth = binWidth;
+        }
+
+        public void Process(Core.DataCollection data)
+        {
+            foreach (DataRow row in data.Rows)
+                row[_columnName] = GenerateBinLabel(row[_columnName] as string);
+        }
+
+        private string GenerateBinLabel(string value)
+        {
+            //Check if strings are int or double and convert then to integers.
+            double dummy;
+            int val = double.TryParse(value, out dummy)
+                            ? (int)Math.Round(double.Parse(value))
+                            : int.Parse(value);
+
+            //Calculate bin bounds .
+            int lowBound = val - (val % _binWidth);
+            int highBound = lowBound + _binWidth - 1;
+            return string.Format("{0} - {1}", lowBound, highBound);
+        }
+    }
+}
--- FinalProject/Project/Program.cs
+++ FinalProject/Project/Program.cs
@@ -28,6 +28,8 @@ namespace Project
             //Treat age column as numeric values.
             manager.Data.SetColumnType("age", DataType.Numeric);
 
+            manager.AddProcessor(new EqualWidthBinLabeling("age", 5));
+
             //Give missing values in occupation column the label 'Unknown'.
             manager.AddProcessor(new ChangeValue("occupation", value => value == "?" ? "Unknown" : value));
             //Give missing values in native-country column the label 'Unknown'.
@@ -35,8 +37,15 @@ namespace Project
 
             //Preprocess data.
             manager.Run();
-
-            //Calculate Apriori threshold from a 1 % support count.
+            foreach (var ageRange in manager.Data["age"]
+                .GroupBy(a => a)
+                .Select(group => new {group.Key, Count = group.Count()})
+                .OrderBy(group => group.Key))
+            {
+                Console.WriteLine(ageRange.Key + ": " + ageRange.Count);
+            }
+            Console.Read();
+            //Calculate Apriori threshold from a 1 % support.
             var threshold = (int)Math.Round(manager.Data.Rows.Count * 0.01);
             var apriori = new AprioriAlgorithm(threshold, "age", "sex", "income", "marital-status");
             var results = apriori.Calculate(manager.Data);
